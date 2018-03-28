1f5a266c66fa8400a6c980dc9626a3e229650566
cristina <crisgalasanchez@gmail.com>
2018-03-28 22:19:40 +0200

    general styles and some size mediaueries

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var page = document.querySelector('.page');
+var navOpen = page.querySelector('.nav-trigger');
+var navClose = page.querySelector('.nav-close');
+
+
+function openMenu() {
+	page.classList.add('page--nav-visible');
+};
+
+function closeMenu() {
+	page.classList.remove('page--nav-visible');
+};
+
+
+navOpen.addEventListener('click', openMenu);
+navClose.addEventListener('click', closeMenu);
